2b250ba1944ba4c5f33f03b9a073f7ce01fd7996
arnica-engineering <95713264+arnica-engineering@users.noreply.github.com>
2026-05-27 23:22:26 -0400

    Create Apache_CVE_2025_54539.cs

---
+++ NuGet/Apache_CVE_2025_54539.cs
@@ -0,0 +1,201 @@
+
+
+public class Apache_CVE_2025_54539 {
+
+    public static void Run(string[] args) {
+    
+        
+//ruleid: nuget.Apache.CVE-2025-54539
+Apache.NMS.AMQP.NmsContext CONSTRUCTOR = fakeObjectGenerator();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsContext();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+Apache.NMS.AMQP.Provider.Amqp.AmqpProvider CONSTRUCTOR = fakeObjectGenerator();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.Provider.Amqp.AmqpProvider();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+NMS.AMQP.Provider.Amqp.Message.TrustedClassFilter CONSTRUCTOR = fakeObjectGenerator();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new NMS.AMQP.Provider.Amqp.Message.TrustedClassFilter();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+NMS.AMQP.Policies.NmsDefaultDeserializationPolicy CONSTRUCTOR = fakeObjectGenerator();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new NMS.AMQP.Policies.NmsDefaultDeserializationPolicy();
+
+Apache.NMS.AMQP.NmsSession A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateConsumerAsync();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsSession().CreateConsumerAsync();
+
+var A = new Apache.NMS.AMQP.NmsSession();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateConsumerAsync();
+
+Apache.NMS.AMQP.NmsConsumer A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.Receive();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsConsumer().Receive();
+
+var A = new Apache.NMS.AMQP.NmsConsumer();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.Receive();
+
+Apache.NMS.AMQP.NmsSession A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsSession().OnConnectionRecovery();
+
+var A = new Apache.NMS.AMQP.NmsSession();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+Apache.NMS.AMQP.Provider.Amqp.Message.TrustedClassFilter A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.BindToType();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.Provider.Amqp.Message.TrustedClassFilter().BindToType();
+
+var A = new Apache.NMS.AMQP.Provider.Amqp.Message.TrustedClassFilter();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.BindToType();
+
+Apache.NMS.AMQP.Message.NmsObjectMessage A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.Body();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.Message.NmsObjectMessage().Body();
+
+var A = new Apache.NMS.AMQP.Message.NmsObjectMessage();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.Body();
+
+Apache.NMS.AMQP.NmsLocalTransactionContext A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsLocalTransactionContext().OnConnectionRecovery();
+
+var A = new Apache.NMS.AMQP.NmsLocalTransactionContext();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+Apache.NMS.AMQP.NmsConnection A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateSession();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsConnection().CreateSession();
+
+var A = new Apache.NMS.AMQP.NmsConnection();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateSession();
+
+Apache.NMS.AMQP.NmsConnection A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsConnection().OnConnectionRecovery();
+
+var A = new Apache.NMS.AMQP.NmsConnection();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+Apache.NMS.AMQP.NmsMessageProducer A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsMessageProducer().OnConnectionRecovery();
+
+var A = new Apache.NMS.AMQP.NmsMessageProducer();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+Apache.NMS.AMQP.NmsConnectionFactory A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateContext();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsConnectionFactory().CreateContext();
+
+var A = new Apache.NMS.AMQP.NmsConnectionFactory();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateContext();
+
+Apache.NMS.AMQP.NmsConnectionFactory A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateConnection();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsConnectionFactory().CreateConnection();
+
+var A = new Apache.NMS.AMQP.NmsConnectionFactory();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateConnection();
+
+Apache.NMS.AMQP.Policies.NmsDefaultDeserializationPolicy A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.IsTrustedType();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.Policies.NmsDefaultDeserializationPolicy().IsTrustedType();
+
+var A = new Apache.NMS.AMQP.Policies.NmsDefaultDeserializationPolicy();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.IsTrustedType();
+
+Apache.NMS.AMQP.NmsMessageConsumer A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.Receive();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsMessageConsumer().Receive();
+
+var A = new Apache.NMS.AMQP.NmsMessageConsumer();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.Receive();
+
+Apache.NMS.AMQP.NmsMessageConsumer A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsMessageConsumer().OnConnectionRecovery();
+
+var A = new Apache.NMS.AMQP.NmsMessageConsumer();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.OnConnectionRecovery();
+
+Apache.NMS.AMQP.NmsSession A = fakeObjectGenerator();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateConsumer();
+
+//ruleid: nuget.Apache.CVE-2025-54539
+new Apache.NMS.AMQP.NmsSession().CreateConsumer();
+
+var A = new Apache.NMS.AMQP.NmsSession();
+//ruleid: nuget.Apache.CVE-2025-54539
+var A_FINAL = A.CreateConsumer();
+
+    
+    }
+
+}
+
