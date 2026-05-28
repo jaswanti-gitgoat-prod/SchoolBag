09bd6829092c37401a133c147fd6f651e3c21437
arnica-engineering <95713264+arnica-engineering@users.noreply.github.com>
2026-05-27 23:21:41 -0400

    Create Amazon_CVE_2025_11573.cs

---
+++ NuGet/Amazon_CVE_2025_11573.cs
@@ -0,0 +1,147 @@
+
+
+public class Amazon_CVE_2025_11573 {
+
+    public static void Run(string[] args) {
+    
+        
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.Internals.Text.TextScanner CONSTRUCTOR = fakeObjectGenerator();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.Text.TextScanner();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.Builders.IonLoader.WithReaderOptions();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.IIonReader CONSTRUCTOR = fakeObjectGenerator();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.IIonReader();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.Builders.IonReaderBuilder.Build();
+
+Amazon.IonDotnet.Internals.SharedSymbolTable A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteTo();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.SharedSymbolTable().WriteTo();
+
+var A = new Amazon.IonDotnet.Internals.SharedSymbolTable();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteTo();
+
+Amazon.IonDotnet.Internals.PrivateIonWriterBase A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteValues();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.PrivateIonWriterBase().WriteValues();
+
+var A = new Amazon.IonDotnet.Internals.PrivateIonWriterBase();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteValues();
+
+Amazon.IonDotnet.Internals.PrivateIonWriterBase A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteContainerRecursively();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.PrivateIonWriterBase().WriteContainerRecursively();
+
+var A = new Amazon.IonDotnet.Internals.PrivateIonWriterBase();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteContainerRecursively();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.Internals.ReaderLocalTable.ImportReaderTable();
+
+Amazon.IonDotnet.Internals.Text.TextRawReader A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.ReadLongString();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.Text.TextRawReader().ReadLongString();
+
+var A = new Amazon.IonDotnet.Internals.Text.TextRawReader();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.ReadLongString();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.Builders.IonLoader.WriteDatagram();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.Internals.ReaderLocalTable.ReadSymbolList();
+
+Amazon.IonDotnet.Internals.Text.TextRawReader A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.ReadClob();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.Text.TextRawReader().ReadClob();
+
+var A = new Amazon.IonDotnet.Internals.Text.TextRawReader();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.ReadClob();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.Internals.ReaderLocalTable.ReadImportList();
+
+Amazon.IonDotnet.Internals.PrivateIonWriterBase A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteValueRecursively();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.PrivateIonWriterBase().WriteValueRecursively();
+
+var A = new Amazon.IonDotnet.Internals.PrivateIonWriterBase();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteValueRecursively();
+
+Amazon.IonDotnet.Internals.PrivateIonWriterBase A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteValue();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.PrivateIonWriterBase().WriteValue();
+
+var A = new Amazon.IonDotnet.Internals.PrivateIonWriterBase();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteValue();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+Amazon.IonDotnet.Builders.IonLoader CONSTRUCTOR = fakeObjectGenerator();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Builders.IonLoader();
+
+Amazon.IonDotnet.Internals.Text.TextRawReader A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.ReadBlob();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.Text.TextRawReader().ReadBlob();
+
+var A = new Amazon.IonDotnet.Internals.Text.TextRawReader();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.ReadBlob();
+
+Amazon.IonDotnet.Internals.SubstituteSymbolTable A = fakeObjectGenerator();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteTo();
+
+//ruleid: nuget.Amazon.CVE-2025-11573
+new Amazon.IonDotnet.Internals.SubstituteSymbolTable().WriteTo();
+
+var A = new Amazon.IonDotnet.Internals.SubstituteSymbolTable();
+//ruleid: nuget.Amazon.CVE-2025-11573
+var A_FINAL = A.WriteTo();
+
+    
+    }
+
+}
+
